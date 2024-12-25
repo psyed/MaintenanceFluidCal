28ecb9a052a1b6fcbd945b592070b6681211e4f4
psyed <psy881212@gmail.com>
2024-12-25 16:23:14 +0000

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function calculate() {
-  const weight = parseFloat(document.getElementById("weight").value);
-  const maintenanceFluid = calculateMaintenanceFluid(weight);
-  document.getElementById("result").textContent = "Maintenance Fluid: " + maintenanceFluid + " mL/day";
-}
